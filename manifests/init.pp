8aa654a75178a20bb945d20e56aebf2e455f79e3
ashrithr <ashrith@me.com>
2013-08-19 11:58:57 -0700

    Update to include default template

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,13 +5,13 @@ class jmxtrans inherits jmxtrans::params {
   case $::operatingsystem {
     'RedHat', 'CentOS': {
       $pkg_provider       = 'rpm'
-      $package_name       = "jmxtrans_${jmxtrans::params::version}.noarch.rpm"
+      $package_name       = "jmxtrans_${jmxtrans::params::redhat_version}.noarch.rpm"
       $tmpsource          = "/tmp/${package_name}"
       $defaults_file_path = '/etc/sysconfig/jmxtrans'
     }
     'Debian', 'Ubuntu': {
       $pkg_provider       = 'dpkg'
-      $package_name       = "jmxtreans_${jmxtrans::params::version}_all.deb"
+      $package_name       = "jmxtreans_${jmxtrans::params::debian_version}_all.deb"
       $tmpsource          = "/tmp/${package_name}"
       $defaults_file_path = '/etc/default/jmxtrans'
     }
@@ -20,13 +20,22 @@ class jmxtrans inherits jmxtrans::params {
     }
   }
 
+  # Variables to build erb
+  $java_home = jmxtrans::params::java_home
+  $seconds_between_run = jmxtrans::params::seconds_between_run
+  $json_dir = jmxtrans::params::json_dir
+  $heap_size = jmxtrans::params::heap_size
+  $new_size = jmxtrans::params::heap_newgen_size
+  $cpu_cores = jmxtrans::params::cpu_cores
+  $new_ratio = jmxtrans::params::new_ratio
+
   file { $tmpsource:
     ensure  => present,
     source  => "puppet:///modules/${module_name}/${package_name}",
     backup  => false,
     owner   => 'root',
     group   => 'root',
-    before  => package[$package_name]
+    before  => Package[$package_name]
   }
 
   package { $package_name:
@@ -35,11 +44,17 @@ class jmxtrans inherits jmxtrans::params {
     provider  => $pkg_provider
   }
 
+  file { "${defaults_file_path}":
+    ensure => present,
+    content => template("jmxtrans/jmxtrans.default.erb"),
+    require => Package[$package_name]
+  }
+
   service { "jmxtrans":
     enable      => true,
     ensure      => running,
     hasrestart  => true,
     hasstatus   => true,
-    require     => Package[$package_name]
+    require     => [Package[$package_name], File[$defaults_file_path]]
   }
 }
